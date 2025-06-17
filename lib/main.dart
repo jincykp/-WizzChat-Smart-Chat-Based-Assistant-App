db80a6948d1e80d295678fbd3cb6eea4fed062d5
Jincy kp <jincykp98@gmail.com>
2025-06-17 11:37:12 +0530

    feat: Firebase setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:wizzchat/firebase_options.dart';
 import 'package:wizzchat/views/home_screen.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
